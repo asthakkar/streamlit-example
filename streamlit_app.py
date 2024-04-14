9da0f8b9b66cca85c8eb44946ef7139b87ff1523
asthakkar <166347576+asthakkar@users.noreply.github.com>
2024-04-13 22:34:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,52 +30,54 @@ return_type_dict = {
     "Rect coords": "box"
 }
 return_type = return_type_dict[return_type_choice]
+div {
+  width: 200px
 
-if img_file:
-    img = Image.open(img_file)
-    #st.image(img, caption="image as is")
-    img = ImageOps.exif_transpose(img)
-    #st.image(img, caption="image with exif off")
-    if not realtime_update:
-        st.write("Double click to save crop")
-    if return_type == 'box':
-        rect = st_cropper(
-            img,
-            realtime_update=realtime_update,
-            box_color=box_color,
-            aspect_ratio=aspect_ratio,
-            return_type=return_type,
-            stroke_width=stroke_width
-        )
-        raw_image = np.asarray(img).astype('uint8')
-        left, top, width, height = tuple(map(int, rect.values()))
-        st.write(rect)
-        masked_image = np.zeros(raw_image.shape, dtype='uint8')
-        masked_image[top:top + height, left:left + width] = raw_image[top:top + height, left:left + width]
-        st.image(Image.fromarray(masked_image), caption='masked image')
-    else:
-        # Get a cropped image from the frontend
-        cropped_img = st_cropper(
-            img,
-            realtime_update=realtime_update,
-            box_color=box_color,
-            aspect_ratio=aspect_ratio,
-            return_type=return_type,
-            stroke_width=stroke_width
-        )
-
-        # Manipulate cropped image at will
-        st.write(":exclamation:Preview:exclamation:")
-        _ = cropped_img.thumbnail((150, 150))
-        st.image(cropped_img)
-        if cropped_img is not None:
-            if st.button("Remove Background"):
-                # Convert the input image to a numpy array
-                input_array = np.array(cropped_img)
-                # Apply background removal using rembg
-                output_array = rembg.remove(input_array)
-                # Create a PIL Image from the output array
-                output_image = Image.fromarray(output_array)
-                st.image(output_image, caption='Image that will be submitted', width=300)
-                image = output_image   
-
+    if img_file:
+        img = Image.open(img_file)
+        #st.image(img, caption="image as is")
+        img = ImageOps.exif_transpose(img)
+        #st.image(img, caption="image with exif off")
+        if not realtime_update:
+            st.write("Double click to save crop")
+        if return_type == 'box':
+            rect = st_cropper(
+                img,
+                realtime_update=realtime_update,
+                box_color=box_color,
+                aspect_ratio=aspect_ratio,
+                return_type=return_type,
+                stroke_width=stroke_width
+            )
+            raw_image = np.asarray(img).astype('uint8')
+            left, top, width, height = tuple(map(int, rect.values()))
+            st.write(rect)
+            masked_image = np.zeros(raw_image.shape, dtype='uint8')
+            masked_image[top:top + height, left:left + width] = raw_image[top:top + height, left:left + width]
+            st.image(Image.fromarray(masked_image), caption='masked image')
+        else:
+            # Get a cropped image from the frontend
+            cropped_img = st_cropper(
+                img,
+                realtime_update=realtime_update,
+                box_color=box_color,
+                aspect_ratio=aspect_ratio,
+                return_type=return_type,
+                stroke_width=stroke_width
+            )
+    
+            # Manipulate cropped image at will
+            st.write(":exclamation:Preview:exclamation:")
+            _ = cropped_img.thumbnail((150, 150))
+            st.image(cropped_img)
+            if cropped_img is not None:
+                if st.button("Remove Background"):
+                    # Convert the input image to a numpy array
+                    input_array = np.array(cropped_img)
+                    # Apply background removal using rembg
+                    output_array = rembg.remove(input_array)
+                    # Create a PIL Image from the output array
+                    output_image = Image.fromarray(output_array)
+                    st.image(output_image, caption='Image that will be submitted', width=300)
+                    image = output_image   
+}
