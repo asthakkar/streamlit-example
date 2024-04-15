c2fa574831b0a8756beae42e350d497b1b91a342
asthakkar <166347576+asthakkar@users.noreply.github.com>
2024-04-14 22:45:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ return_type_dict = {
     "Rect coords": "box"
 }
 return_type = return_type_dict[return_type_choice]
-intl_message = '<p color:Red; font-size: 4px;">Intl Bhaktiferi only applicable from North America but excluding to India.</p>'
-st.markdown(intl_message, unsafe_allow_html=True)
+intl_message = '<p "color:Red; font-size: 4px;">Intl Bhaktiferi only applicable from North America but excluding to India.</p>'
+st.markdown(:red[intl_message], unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 with col1:
