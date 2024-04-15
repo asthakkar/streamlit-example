4d6f672b391d2e80db15b127aa3caf2ba1bd6d2d
asthakkar <166347576+asthakkar@users.noreply.github.com>
2024-04-15 17:23:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,7 @@ if img_file:
     if ext == ".png":
         st.write("This is a png file")
         img = img.convert("RGB")
+        img = fn + ".jpg"
         st.write("after convert")
         st.write(img)
     #st.image(img, caption="image with exif off")
