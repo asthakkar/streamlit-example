a805338877fd761790aab61b4a07e287403005e5
asthakkar <166347576+asthakkar@users.noreply.github.com>
2024-04-15 17:12:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ if intl_from:
         input_data[field_key] = f"{intl_from} to {intl_to}"
 
 if img_file:
-    image = Image.open(img_file)
+    img = Image.open(img_file)
     #st.image(img, caption="image as is")
-    image = ImageOps.exif_transpose(image)
+    img = ImageOps.exif_transpose(img)
     ext = os.path.splitext(img_file.name)[-1].lower()
     fn = os.path.splitext(img_file.name)[0]
     st.write(ext)
