93091706ec8c504689c8e233be4f89f80f884f76
asthakkar <166347576+asthakkar@users.noreply.github.com>
2024-04-16 13:43:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ return_type = return_type_dict[return_type_choice]
 #         input_data[field_key] = f"{intl_from} to {intl_to}"
 def example():
     rain(
-        emoji=":star:",
+        emoji="⭐",
         font_size=54,
         falling_speed=5,
         animation_length="10",
@@ -60,7 +60,7 @@ if option:
     if option == "Balloons":
         st.balloons()
     elif option == "Toast":
-         st.toast('Thank you.  Your information is submitted!', icon=':thumbsup:')
+         st.toast('Thank you.  Your information is submitted!', icon='👍')
     elif option == "Rain":
         example()
     else:
